88a76c78fed46c5086d667a670e19df2d3210d5c
pabvlov <pablojavierprietocepeda@gmail.com>
2025-08-03 02:25:08 -0400

    cambio de puerto

--- src/GPInventory.Api/Program.cs
+++ src/GPInventory.Api/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", builder =>
     {
-        builder.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://localhost:7237", "https://localhost:5173", "http://localhost:5000")
+        builder.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://localhost:5001", "https://localhost:5173", "http://localhost:5000")
                .AllowAnyHeader()
                .AllowAnyMethod()
                .AllowCredentials();
